864d60e4fa2dd40ba10cff3a0a455d66a7b6b848
Richard Ruehl <richardruehl@pm.me>
2024-02-18 17:31:09 +0100

    simple load texts from json file application

---
+++ src/components/Content.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from 'vue'
+
+defineProps({
+  data: Object
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ data.title }}</h1>
+
+  <h2>{{ data.subtitle }}</h2>
+
+  <p>{{ data.text }}</p>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
